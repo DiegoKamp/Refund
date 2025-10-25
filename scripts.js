bde55938a8d680a7091ac1c8d830fec818c29ac1
DiegoKamp <diegokamp@gmail.com>
2025-10-25 03:28:34 -0300

    Contador de itens funcionando

--- scripts.js
+++ scripts.js
@@ -9,9 +9,6 @@ const category = document.getElementById("category");
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul");
 
-
-
-
 // Regex para aceitar apenas input numéricos
 amount.oninput = () => {
 
@@ -25,7 +22,6 @@ amount.oninput = () => {
   amount.value = formatCurrencyBRL(value);
 
   //  console.log(value);
-
 
 }
 
@@ -55,7 +51,6 @@ form.onsubmit = (event) => {
   }
   //console.log(newExpense)
 
-
   expenseAdd(newExpense)
 }
 
@@ -81,7 +76,6 @@ function expenseAdd(newExpense) {
     const expenseInfo = document.createElement("div");
     expenseInfo.classList.add("expense-info");
 
-
     const expenseName = document.createElement("strong");
     expenseName.textContent = newExpense.expense;
 
@@ -92,51 +86,84 @@ function expenseAdd(newExpense) {
     const expenseAmount = document.createElement("span");
     expenseAmount.classList.add("expense-amount");
     expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
-
     // expenseAmount.innerHTML = `<small>R$</small>${amount.value}`
     // Não seria mais fácil escrever dessa forma ?
 
     // Criação do ícone remover
+    const removeImage = document.createElement("img");
+    removeImage.classList.add("remove-icon")
+    removeImage.setAttribute("src", "./img/remove.svg")
+    removeImage.setAttribute("alt", "remover")
+
+    // Criação do Contador de número de despesas
+    /* 
+    const expenseCount = document.querySelector("aside");
+    expenseCount = ""
+    */
 
 
-
-    // Adiciona o item na lista
+    // Adiciona informações ao item
     expenseItem.append(expenseIcon); // Adicionando o ícone ao Item
     expenseInfo.append(expenseName, expenseCategory) // Adicionando o nome e a categoria à despesa
     expenseItem.append(expenseInfo); // Adicionando a informação com nome e categoria ao Item
     expenseItem.append(expenseAmount); // Adicionando o valor ao Item
+    expenseItem.append(removeImage); // Adicionando o ícone de remover
+
+    // Adiciona o Item na lista
     expenseList.append(expenseItem); // Adicionando o Item à ul
 
-
-
     /*
-        // Usando o innerHTML
-    
-        const expenseItem = document.createElement("li");
-        expenseItem.classList.add("expense");
-        expenseItem.innerHTML =
-          `
-          <img src="img/${newExpense.category_id}.svg" alt="Ícone de despesa do tipo ${newExpense.category_name}" />
-    
-          <div class="expense-info">
-            <strong>${newExpense.expense}</strong>
-            <span>${newExpense.category_name}</span>
-          </div>
-    
-          <span class="expense-amount"><small>R$</small>${newExpense.amount}</span>
-    
-          <img src="./img/remove.svg" alt="remover" class="remove-icon" />
-          `
-        expenseList.append(expenseItem);
-    
-    */
+    // Usando o innerHTML
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+    expenseItem.innerHTML =
+           `
+           <img src="img/${newExpense.category_id}.svg" alt="Ícone de despesa do tipo ${newExpense.category_name}" />
+           <div class="expense-info">
+             <strong>${newExpense.expense}</strong>
+             <span>${newExpense.category_name}</span>
+           </div>
+           <span class="expense-amount"><small>R$</small>${newExpense.amount}</span>
+           <img src="./img/remove.svg" alt="remover" class="remove-icon" />
+           `
+           expenseList.append(expenseItem);
+     */
 
+
+
+
+
+
+    updateTotals()
 
   } catch (error) {
     alert("Não foi possível lançar a despesa")
     console.log(error)
   }
 }
+
+
+// Atualiza os dados totais
+function updateTotals() {
+  try {
+
+    // Recupera todos os itens (li) da lista
+    const items = expenseList.children
+    console.log(items);
+
+    // Atualiza o contador de itens
+    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    // Outra forma de fazer
+    // if (items.length <= 1) { expenseQuantity.textContent = `${items} despesas` } else { expenseQuantity.textContent = `${items} despesa` }
+
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível atualizar os totais.")
+  }
+}
+
+const expenseQuantity = document.querySelector("aside header p span");
 
 
 
